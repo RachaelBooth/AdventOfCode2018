cadd1dad2e0409e2990ebbebdbc65fd4b1e19401
Rachael Booth <rachaelmsb@gmail.com>
2018-12-01 09:31:59 +0000

    Remove template solve() return type restriction

--- src/template/part1.ts
+++ src/template/part1.ts
@@ -4,7 +4,7 @@ import * as _ from 'lodash';
 const day = 0;
 const inputParser = new InputParser(day);
 
-export function solve(): string {
+export function solve() {
     const input = inputParser.readInputLines();
-    return '';
+    return 'not implemented';
 }
